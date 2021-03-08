1b89b5f9198f330efa1b76b923abd40d770b6f9b
Charles BENTEIN <c.bentein@rubika-edu.com>
2021-03-08 17:09:46 +0100

    Cam checkpoint track wallah
    fin de journée tamer

---
+++ NeuralIntensive/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 cameraLocalPosition;
+    public Vector3 localTargetLocalPosition;
+
+    public float positionLerpSpeed = 0.01f;
+    public float lookLerpSpeed = 0.01f;
+
+    Vector3 wantedPos;
+    Quaternion wantedRotation;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        wantedPos = target.TransformPoint(cameraLocalPosition);
+        wantedPos.y = cameraLocalPosition.y + target.position.y;
+
+        transform.position = Vector3.Lerp(transform.position, wantedPos, positionLerpSpeed);
+
+        wantedRotation = Quaternion.LookRotation(target.TransformPoint(localTargetLocalPosition) - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, lookLerpSpeed);
+    }
+}
---
+++ NeuralIntensive/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Transform nextCheckpoint;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.transform.parent.GetComponent<Agent>())
+        //{
+        //    if (other.transform.parent.GetComponent<Agent>.nextCheckpoint == transform)
+        //        other.transform.parent.GetComponent<Agent>.CheckpointReached(nextCheckpoint);
+        //}
+    }
+}
