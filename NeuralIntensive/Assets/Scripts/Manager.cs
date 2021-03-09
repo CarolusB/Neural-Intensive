8c7ccca2fe2e31b074d420d4489a3ba732d0a6bf
Charles BENTEIN <c.bentein@rubika-edu.com>
2021-03-09 16:16:11 +0100

    Manager bgein

---
+++ NeuralIntensive/Assets/Scripts/Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Manager : MonoBehaviour
+{
+    public int populationSize;
+    public float trainingDuration = 30;
+    public float mutationRate = 5;
+
+    public Agent agentPrefab;
+    public Transform agentGroup;
+
+    Agent agent;
+
+    List<Agent> agents = new List<Agent>();
+
+    public CameraController cameraController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator InitCoroutine()
+    {
+        yield return null;
+    }
+
+}
