26b23085bccad922d6d87fd44924d1b3fecbeb45
Charles BENTEIN <c.bentein@rubika-edu.com>
2021-03-10 10:13:55 +0100

    Too much Rigidbodys

--- NeuralIntensive/Assets/Scripts/Manager.cs
+++ NeuralIntensive/Assets/Scripts/Manager.cs
@@ -22,6 +22,7 @@ public class Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        CheckpointManager.instance.Init();
         StartCoroutine(InitCoroutine());
     }
 
